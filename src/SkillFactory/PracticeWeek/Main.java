0d1a11dbfb209d4b364639fbc927b93e67b15528
Castomer2000 <rainofcastomer@gmail.com>
2024-06-24 01:26:11 +0300

    Two

--- src/SkillFactory/PracticeWeek/Main.java
+++ src/SkillFactory/PracticeWeek/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] a = {{3, 8}, {8, 9}, {9, 7}};
+        int[][] a = {{3, 8}, {8, 9}, {9, 7},{96, 7}};
         ToLine toLine = new ToLine(a);
         System.out.println(Arrays.toString(toLine.resize()));
 
