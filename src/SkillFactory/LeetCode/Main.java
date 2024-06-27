19b293464214ab3af74f7dbbabac5a0b11ce4d0f
Castomer2000 <rainofcastomer@gmail.com>
2024-06-27 23:33:36 +0300

    LeetCodeQuest

--- src/SkillFactory/LeetCode/Main.java
+++ src/SkillFactory/LeetCode/Main.java
@@ -4,7 +4,6 @@ import SkillFactory.module4.SRect.Rect;
 
 public class Main {
     public static void main(String[] args) {
-        Rect rect = new Rect(20, 15);
-        System.out.println(rect.getS());
+        Work work = new Work();
     }
 }
--- src/SkillFactory/module4/Test.java
+++ src/SkillFactory/module4/Test.java
@@ -1,10 +1,9 @@
 package SkillFactory.module4;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        String name = "вова";
-        int num = 10;
-        System.out.printf("Привет %s, купи %d яблок", name, num );
 
     }
 }
