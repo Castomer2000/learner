db58482748ea8f6ed21a988f4a4a6dde506b5376
Castomer2000 <rainofcastomer@gmail.com>
2024-07-01 00:10:38 +0300

    NewCommit

--- src/SkillFactory/PracticeWeek/Main.java
+++ src/SkillFactory/PracticeWeek/Main.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] a = {{3, 8}, {8, 9}, {9, 7},{96, 7}};
-        ToLine toLine = new ToLine(a);
-        System.out.println(Arrays.toString(toLine.resize()));
-
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.newGame();
     }
 }
